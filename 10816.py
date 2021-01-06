c07be9d5354d0536aa69ce74974785328650a469
Dongsik <ret0422@gmail.com>
2021-01-06 22:29:12 +0900

    2021. 01. 06. baekjoon problem solving...

---
+++ 10816.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline())
+b = [0for i in range(N)]
+
+M = int(sys.stdin.readline())
+a = [0for i in range (M)]
+
+
+for i in b:
+    for j in a:
+        if(i == j):
